0f3fd535da1596a2a178d82d5810ed1044af8033
singhshivendra95916-max <singhshivendra95916@gmail.com>
2026-06-04 15:39:54 +0530

    ques 32 done and Day_8 completed

---
+++ Day_8/Q32.cpp
@@ -0,0 +1,19 @@
+//Repeated Number Pattern
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int r;
+
+    cout << "Enter the number of rows:";
+    cin >> r;
+
+    for (int i = 1; i <= r; i++) {
+        for (int j = 1; j <= i; j++) {      
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
