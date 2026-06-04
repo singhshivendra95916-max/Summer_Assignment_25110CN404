613b53ffc40cccf166cd5786b23a438041b7fb65
singhshivendra95916-max <singhshivendra95916@gmail.com>
2026-06-04 18:00:57 +0530

    ques 36 done and Day_9 completed

---
+++ Day_9/Q36.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+
+    cout << "Enter the number of rows: ";
+    cin >> r;
+    cout << "Enter the number of columns: ";
+    cin >> c;
+
+    for(int i = 1; i <= r; i++){
+        for(int j = 1; j <= c; j++){
+            if(i == 1 || i == r || j == 1 || j == c){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+}
