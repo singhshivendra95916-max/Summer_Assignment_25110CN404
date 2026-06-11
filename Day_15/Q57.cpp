8ecec57584d0b42a51966243a3fd496b7e06fa2c
singhshivendra95916-max <singhshivendra95916@gmail.com>
2026-06-11 22:02:01 +0530

    ques 57 completed

---
+++ Day_15/Q57.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void inputArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+}
+
+void displayArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements : ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    inputArray(arr, n);
+
+    int i = 0;
+    int j = n - 1;
+    while(i < j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+
+    cout << "Array elements are: ";
+    displayArray(arr, n);
+
+    return 0;
+}
