53e00b6d8f97a5bba96d434ecddb8b604ff74f8f
singhshivendra95916-max <singhshivendra95916@gmail.com>
2026-06-16 22:23:39 +0530

    ques 62 completed

---
+++ Day_16/Q62.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxFreq = 0;
+    int maxElement;
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if (count > maxFreq) {
+            maxFreq = count;
+            maxElement = arr[i];
+        }
+    }
+
+    cout << "Maximum frequency element = " << maxElement << endl;
+    cout << "Frequency = " << maxFreq << endl;
+
+    return 0;
+}
