7524283f65aedbe97598abce28752ac7ef29c8ee
singhshivendra95916-max <singhshivendra95916@gmail.com>
2026-06-18 22:55:06 +0530

    ques 71 completed

---
+++ Day_18/Q71.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements in sorted order: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter element to search: ";
+    cin >> target;
+
+    int low = 0, high = n - 1;
+    bool found = false;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target) {
+            cout << "Element found at index " << mid << endl;
+            found = true;
+            break;
+        }
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (!found) {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
