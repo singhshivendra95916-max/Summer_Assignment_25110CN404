5e2b72cdf1bffe5cb636ec3709dcd80d0dd79576
singhshivendra95916-max <singhshivendra95916@gmail.com>
2026-06-18 23:00:36 +0530

    que 72 done and Day_18 completed

---
+++ Day_18/Q72.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i <= n - 2; i++) {
+        for (int j = i + 1; j <= n - 1; j++) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "Array in descending order: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
