b531cb7a71e29055b7336b41ccdd9562afd8c8a3
singhshivendra95916-max <singhshivendra95916@gmail.com>
2026-06-19 17:53:23 +0530

    ques 73 completed

---
+++ Day_19/Q73.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, columns;
+
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> columns;
+
+    int A[10][10], B[10][10], Sum[10][10];
+
+    cout << "Enter elements of first matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {                           // adding both matrices
+        for (int j = 0; j < columns; j++) {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    cout << "Sum of the matrices:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cout << Sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
